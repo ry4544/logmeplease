dc57cbf24bc98c8b09276bbb4035af0057a468da
ry4544 <73912553+ry4544@users.noreply.github.com>
2020-11-03 16:12:38 -0500

    Add files via upload

---
+++ LogCreaterApp/LogCreaterApp/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace LogCreaterApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txt_box1.Text = "Choose save location";
+
+        }
+
+        private void SaveStart(string arg)
+        {
+            try
+            {
+                
+                ProcessStartInfo psi = new ProcessStartInfo("cmd.exe");
+                psi.UseShellExecute = true;
+                psi.WindowStyle = ProcessWindowStyle.Normal;
+                psi.Verb = "runas";
+                psi.Arguments = arg;
+                Process.Start(psi);
+                
+                
+                
+                
+                MessageBox.Show("Complete");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveStart("netstat -a >"+ txt_box1.Text);
+
+        }
+    }
+}
